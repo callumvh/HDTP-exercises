362041c858c71234da152013c3f10cc55a0016f9
Callum van Heerden <callumvanheerden@gmail.com>
2022-07-12 23:00:25 +0200

    progrss

--- Sections/2.5-Programs.rkt
+++ Sections/2.5-Programs.rkt
@@ -1,6 +1,34 @@
-#lang htdp/bsl+
+  #lang htdp/bsl+
 
-(require 2htdp/batch-io)
+  (require 2htdp/batch-io)
+  (require 2htdp/image)
+  (require 2htdp/universe)
+
+  (write-file "calInput.txt" "212")
+
+  (define (C f)
+    (* 5/9 (- f 32)))
+
+  (define (convert in out)
+    (write-file out
+                (string-append
+                (number->string
+                  (C
+                  (string->number
+                    (read-file in))))
+                "\n")))
+
+  (convert "calInput.txt" "calOutput.txt")
 
 
-(write-file
+  (define (number->square s)
+    (square s "solid" "red"))
+
+  (number->square 5)
+
+#| (big-bang 100 [to-draw number->square]) |#
+
+(big-bang 100
+    [to-draw number->square]
+    [on-tick sub1]
+    [stop-when zero?])
